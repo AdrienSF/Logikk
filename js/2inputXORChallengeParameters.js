f8dd30335bf75c2a07272f55cf1352270254a0b3
Adrien Schurger-Foy <adrien.schurger-foy@student.manchester.ac.uk>
2018-03-15 17:58:02 +0000

    challenge limiters are specified in url

--- js/2inputXORChallengeParameters.js
+++ js/2inputXORChallengeParameters.js
@@ -1,8 +1,20 @@
-goalInputs = 2;
-maxANDgates = 2;
-maxORgates = 2;
-maxXORgates = 0;
-maxNOTgates = 2;
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+goalInputs = getParameterByName("in");
+maxANDgates = getParameterByName("and");
+maxORgates = getParameterByName("or");
+maxXORgates = getParameterByName("xor");
+maxNOTgates = getParameterByName("not");
 goalTable = [false, true, true, false];//2 input XOR truth table out column
 winMessage = "congratulations, you have solved one of the 16 challenges of the lowest complexity";
 
