d75f1242d426d2aef8bfce073cd177791feb01b4
Adrien Schurger-Foy <adrien.schurger-foy@student.manchester.ac.uk>
2018-03-18 17:10:27 +0000

    temp

---
+++ js/3inputTrueChallengeParameters.js
@@ -0,0 +1,13 @@
+goalInputs = 3;
+maxANDgates = 2;
+maxORgates = 2;
+maxXORgates = 2;
+maxNOTgates = 2;
+goalTable = [true, true, true, true, true, true, true, true];//all true
+winMessage = "congratulations, you have solved one of the 256 challenges of the second lowest complexity";
+
+document.getElementById("goalInputs").innerHTML = goalInputs;
+document.getElementById("maxANDgates").innerHTML = maxANDgates;
+document.getElementById("maxORgates").innerHTML = maxORgates;
+document.getElementById("maxXORgates").innerHTML = maxXORgates;
+document.getElementById("maxNOTgates").innerHTML = maxNOTgates;
