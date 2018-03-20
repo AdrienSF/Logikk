1873e6b3b73bca2ec32995783650747b65ac945c
Ivan Donat Pupovac <ivan.pupovac@student.manchester.ac.uk>
2018-03-20 13:17:28 +0000

    Favicon fix

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
   <title>LogiKK</title>
 
   <!-- Bootstrap CSS CDN -->
