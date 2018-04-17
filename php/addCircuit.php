09550372c4409a294c2d18b659d1889917d22bc6
Burhanuddin Salim <burhanuddin.salim@student.manchester.ac.uk>
2018-04-17 19:21:30 +0100

    returns message of status

--- php/addCircuit.php
+++ php/addCircuit.php
@@ -5,8 +5,18 @@
   $circuitText = $_POST['circuit'];
   $circuitName = test_input($_POST['name']);
 
+  $queryCheckName = "SELECT * FROM save_circuits WHERE "
+
   $queryCircuitAdd = "INSERT INTO save_circuits VALUES ('".$_SESSION['username']."','".$circuitName."','".$circuitText."')";
   $res = $mysql->query($queryCircuitAdd);
+
+  if ($res) {
+    echo "Your circuit has been added";
+  } else {
+    echo "Sorry something went wrong";
+  }
+
+  $mysql->close();
 
   function test_input($data)
   {
